c92bf60dfd360658ab0d8e92defb22549c39b5ab
eliassouza93 <elias99-souza@hotmail.com>
2025-05-15 19:12:29 -0300

    task

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ const server = http.createServer(async (req, res) => {
 
     if (route) {
         const routeParams = req.url.match(route.path);
-        req.params = { ...routeParams.groups }; // 🔹 Agora corretamente atribui req.params
+        req.params = { ...routeParams.groups }; 
 
         return route.handler(req, res);
     }
