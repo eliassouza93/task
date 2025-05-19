75cc72369d91bde5a455416fbf97478e663fef3d
eliassouza93 <elias99-souza@hotmail.com>
2025-05-19 18:31:49 -0300

    csv

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,30 @@
 import { Database } from "./database.js"
 import { randomUUID } from 'node:crypto'
 import { buildRoutePath } from "./utils/build-route-path.js"
+import fs from 'node:fs'
+
+function exportTaskCSV(tasks) {
+    const header = 'id,title,description\n'
+    const rows = tasks.map(task => `${task.id},"${task.title}","${task.description}"`).join('\n')
+    const csvContainer = header + rows
+
+    fs.writeFileSync('./tasks.csv', csvContainer, 'utf-8')
+
+}
+
+function deleteTaskCSV(tasks) {
+    const header = 'id,title,description\n'
+    const rows = tasks.map(task => `${task.id},"${task.title}","${task.description}"`).join('\n')
+    const csvContent = header + rows
+
+    fs.writeFileSync('./tasks.csv', csvContent, 'utf-8')
+}
 
 const database = new Database()
 
 export const routes = [
     {
+
         method: 'GET',
         path: buildRoutePath('/task'),
         handler: (req, res) => {
@@ -32,6 +51,11 @@ export const routes = [
                 description,
             }
             database.insert('task', newTask)
+
+            const allTasks = database.select('task')
+            exportTaskCSV(allTasks)
+
+
             return res.writeHead(201).end(JSON.stringify(newTask))
         }
     },
@@ -50,13 +74,16 @@ export const routes = [
         handler: (req, res) => {
             const { id } = req.params;
             const task = database.select('task').find(task => task.id === id);
-    
+
             if (!task) {
                 return res.writeHead(404).end(JSON.stringify({ message: 'Task not found' }));
             }
-    
+
             database.delete('task', id)
-    
+
+              const remainingTasks = database.select('task')
+        deleteTaskCSV(remainingTasks)
+            
             return res.writeHead(204).end()
         }
     }
